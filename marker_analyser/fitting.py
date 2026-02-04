ca495214978c2172606e60debddd5131700753be
SylviaWhittle <sylviwhittle@gmail.com>
2026-02-04 01:42:47 +0000

    Fitting: fix model not fitting when no initial values are provided

--- marker_analyser/fitting.py
+++ marker_analyser/fitting.py
@@ -49,13 +49,22 @@ def fit_model_to_data(
     model = model(name="fit") + pylake.force_offset(name="fit")
     fit = pylake.FdFit(model)
     fit.add_data(name="data", f=forces, d=distances)
-    fit["fit/Lp"].value = lp_value  # Initial guess for persistence length in nm
-    fit["fit/Lp"].lower_bound = lp_lower_bound  # Lower bound for persistence length in nm
-    fit["fit/Lp"].upper_bound = lp_upper_bound  # Upper bound for persistence length in nm
-    fit["fit/Lc"].value = lc_value  # Initial guess for contour length in nm
-    fit["fit/Lc"].value = lc_value  # Initial guess for contour length in nm
-    fit["fit/f_offset"].lower_bound = force_offset_lower_bound  # Lower bound for force offset
-    fit["fit/f_offset"].upper_bound = force_offset_upper_bound  # Upper bound for force offset
+    if lp_value is not None:
+        fit["fit/Lp"].value = lp_value  # Initial guess for persistence length in nm
+    if lp_lower_bound is not None:
+        fit["fit/Lp"].lower_bound = lp_lower_bound  # Lower bound for persistence length in nm
+    if lp_upper_bound is not None:
+        fit["fit/Lp"].upper_bound = lp_upper_bound  # Upper bound for persistence length in nm
+    if lc_value is not None:
+        fit["fit/Lc"].value = lc_value  # Initial guess for contour length in nm
+    if lc_value is not None:
+        fit["fit/Lc"].value = lc_value  # Initial guess for contour length in nm
+    if force_offset_lower_bound is not None:
+        fit["fit/f_offset"].lower_bound = force_offset_lower_bound  # Lower bound for force offset
+    if force_offset_upper_bound is not None:
+        fit["fit/f_offset"].lower_bound = force_offset_lower_bound  # Lower bound for force offset
+    if force_offset_upper_bound is not None:
+        fit["fit/f_offset"].upper_bound = force_offset_upper_bound  # Upper bound for force offset
     # Perform the fit
     fit.fit()
     # Note that the error for some reason is the same value repeated for each point. Let's take
